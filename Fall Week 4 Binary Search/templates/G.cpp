7ef8f73c7395d360ee7a9fccefd0b2264f24cf2f
Oistrachmaninoff <40538303+stringlotte@users.noreply.github.com>
2024-10-25 05:48:56 +0800

    Template for G

---
+++ Fall Week 4 Binary Search/templates/G.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve()
+{
+    // read the input
+    long long n, s;
+    cin >> n >> s;
+
+    // your code here
+    
+}
+
+int main()
+{
+    int cases;
+    cin >> cases;
+    for (int i=0; i<cases; i++)
+        solve();
+    return 0;
+}
