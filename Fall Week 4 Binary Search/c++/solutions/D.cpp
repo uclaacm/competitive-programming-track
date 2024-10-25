e0e415bf726bcc3469400e3c7a99e567d6de21ee
anpoli99 <47492849+anpoli99@users.noreply.github.com>
2024-10-24 20:10:39 -0700

    Update D.cpp

--- Fall Week 4 Binary Search/c++/solutions/D.cpp
+++ Fall Week 4 Binary Search/c++/solutions/D.cpp
@@ -1,28 +1,56 @@
-#include <bits/stdc++.h>
-using namespace std; 
+#include "bits/stdc++.h"
+using namespace std;
 
-void solve(){
-    // read the input
-    int l, r;
-    cin >> l >> r;
 
-    // greedy: create the array with the smallest different between the smallest and largest elements
-    vector<int> a;
-    a.push_back(l);
-    int increment = 1;
+/*
+This problem can be implemented with binary search, as if we can't afford 'k' elements,
+we know 'k + 1' elements will be more expensive, so we can always elimiate half of the search space.
 
-    while (a.back() + increment <= r){
-        a.push_back(a.back() + increment);
-        increment++;
-    }
-
-    cout << a.size() << endl;
-}
-
+For this problem, since the costs are always changing, we need to update the cost
+array each iteration. After updating the cost array, we can find the minimum cost
+to select 'k' elements by sorting the updated cost array.
+*/
 int main(){
-    int cases;
-    cin >> cases;
-    for(int c = 0; c < cases; c++){
-        solve();
+    int n; long long s; 
+    cin >> n >> s; 
+    vector<long long> a(n); // We need long longs! The numbers become too big to be stored in an int
+    for(int i = 0; i < n; i++){
+        cin >> a[i];
     }
+    int lo = 0, hi = n;
+    while(hi - lo > 3){
+        int mid = (lo + hi) / 2;
+        vector<long long> costs(n);
+        for(int i = 0; i < n; i++){
+            costs[i] = a[i] + 1ll * mid * (i + 1); // The problem-statement is 1-indexed, so add 1
+        }
+        sort(costs.begin(), costs.end()); // Now we have the sorted costs, so we can find the minimum cost of 'mid' elements
+        long long sum = 0;
+        for(int i = 0; i < mid; i++){
+            sum += costs[i];
+        }
+        if(sum <= s){
+            lo = mid; // We can afford 'mid' elements, so we can try for more
+        } else {
+            hi = mid; // We can't afford 'mid' elements, so we need to try less
+        }
+        
+    }
+    for(int k = hi; k >= lo; k--){ // Print the maximum number in range we can afford
+
+        vector<long long> costs(n);
+        for(int i = 0; i < n; i++){
+            costs[i] = a[i] + k * (i + 1); // The problem-statement is 1-indexed, so add 1
+        }
+        sort(costs.begin(), costs.end()); // Now we have the sorted costs, so we can find the minimum cost of 'mid' elements
+        long long sum = 0;
+        for(int i = 0; i < k; i++){
+            sum += costs[i];
+        }
+        if(sum <= s){
+            cout << k << ' ' << sum << endl; // We can afford 'k' elements!
+            break;
+        }
+    }
+
 }
