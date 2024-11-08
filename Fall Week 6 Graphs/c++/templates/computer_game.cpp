7b0d94a9d8bded03251e6ab7d7fe1d414653a841
flaming-snowman <a.flaming.snowman@gmail.com>
2024-11-07 19:37:15 -0800

    Add template for B. Computer Game

---
+++ Fall Week 6 Graphs/c++/templates/computer_game.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+void solve() {
+    int N;
+    cin>>N;
+    string row1, row2;
+    cin >> row1 >> row2;
+    bool win = true;
+    // figure out if you can win or not
+    if (win) cout << "YES" << endl;
+    else cout << "NO" << endl;
+}
+
+int main() {
+    int cases;
+    cin >> cases;
+    for(int i = 0; i < cases; i++) {
+        solve();
+    }
+}
