f1f74668ccd26eb39212cb09e68b2bd56767c8b8
anpoli99 <47492849+anpoli99@users.noreply.github.com>
2024-11-14 18:17:54 -0800

    Create doubleplusgood.cpp

---
+++ Fall Week 7 Recursion/c++/templates/doubleplusgood.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string s;
+// Store all possible number in 'ans'; we provide template code for making them unique
+vector<int> ans;
+void solve(int pos, int curnum, int sum){
+    // What should the base case be?
+    
+    // Recursive function to solve:
+    // pos is position in the string
+    // Remember: the numerical value of the character at s[pos] is int(s[pos] - '0')
+
+}
+int main(){
+    string s;
+    cin >> s;
+    solve(0, 0, 0);
+    sort(ans.begin(), a.end());
+    ans.erase(unique(ans.begin(), ans.end()), ans.end());
+    cout << ans.size() << '\n';
+}
