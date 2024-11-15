240244d6813d208e254bef8c1d1ec5aa120874d3
anpoli99 <47492849+anpoli99@users.noreply.github.com>
2024-11-14 18:31:26 -0800

    Update doubleplusgood.cpp

--- Fall Week 7 Recursion/c++/templates/doubleplusgood.cpp
+++ Fall Week 7 Recursion/c++/templates/doubleplusgood.cpp
@@ -1,22 +1,38 @@
-#include <bits/stdc++.h>
+#include <algorithm>
+#include <iostream>
+#include <sstream>
+#include <vector>
 using namespace std;
-// Under construction :) 
-string s;
-// Store all possible number in 'ans'; we provide template code for making them unique
-vector<int> ans;
-void solve(int pos, int curnum, int sum){
-    // What should the base case be?
-    
-    // Recursive function to solve:
-    // pos is position in the string
-    // Remember: the numerical value of the character at s[pos] is int(s[pos] - '0')
 
+// The numbers in the input
+vector<string> nums; 
+// Store all possible numbers in 'ans'; we provide template code for making them unique
+vector<long long> ans;
+void solve(int pos, string curnum, long long sum){
+    // Reminder: to convert a string 's' to a long long int, use stoll(s)
+    // We need long long ints because there can be 18 digits, but a long long int is large enough to store the answer
+    // What base case goes here
+
+
+    // Your recursive calls here
 }
 int main(){
+    /* Reading and parsing the input*/
     string s;
     cin >> s;
-    solve(0, 0, 0);
-    sort(ans.begin(), a.end());
+    for(char& c : s) if(c == '+') c = ' ';
+    stringstream ss(s);
+    string tmp;
+    while(ss >> tmp){
+        nums.push_back(tmp);
+    }
+    /* End input*/
+
+    // Begin recusion
+    solve(0, "", 0); 
+
+    // Find unique elements and print answer  
+    sort(ans.begin(), ans.end());
     ans.erase(unique(ans.begin(), ans.end()), ans.end());
     cout << ans.size() << '\n';
 }
