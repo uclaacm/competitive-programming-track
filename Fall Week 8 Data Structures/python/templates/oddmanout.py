e8f4e8facd4ca6121ac075d4e332fb79afd9f762
anpoli99 <47492849+anpoli99@users.noreply.github.com>
2024-11-21 18:09:07 -0800

    Create oddmanout.py

---
+++ Fall Week 8 Data Structures/python/templates/oddmanout.py
@@ -0,0 +1,19 @@
+
+
+from collections import defaultdict
+# Hint! It might be easier to implement this problem with a defaultdict
+# For example, 
+# mydict = defaultdict(int)
+# This is a map (dict) where if a key is not present, it's "defaults" to 0
+
+qr = int(input())
+for tc in range(qr):
+    print("Case #{}:".format(tc+1), end=" ") # prints "Case #x "
+    n = int(input())
+    a = list(map(int, input().split()))
+    # A now contains the list of all elements
+    # Your code here!
+    
+    pass
+
+    print(ans) # 'ans' should store the answer to the problem here
