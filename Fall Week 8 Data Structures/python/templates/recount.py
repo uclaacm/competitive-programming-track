3ef9f730c1dffea588a34f4838be992a6a0b97ef
anpoli99 <47492849+anpoli99@users.noreply.github.com>
2024-11-21 18:17:00 -0800

    Create recount.py

---
+++ Fall Week 8 Data Structures/python/templates/recount.py
@@ -0,0 +1,17 @@
+
+
+
+while True:
+    name = input()
+    if name == '***':
+        # End of input; stop reading
+        break
+
+    # Here 'name' contains the name of the candidate who received a vote
+
+    # Process the name here
+
+
+
+# Your code here to determine who got the most votes; 
+# Or to print 'Runoff!' in case of a tie!
